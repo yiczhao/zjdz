a2a523f5d195e52853a6ea815b07bbc95710f496
yiczhao <a462812449@gmail.com>
2016-06-17 00:00:57 +0800

    first commit

---
+++ js/login.js
@@ -0,0 +1,11 @@
+/**
+ * Created by yiczhao on 2016/6/16.
+ */
+$('.login-btn').on('click',function(){
+    var _name=$('input[name="username"]');
+    var _password=$('input[name="password"]');
+    console.log(_name.val())
+    console.log(_password.val())
+});
+
+
